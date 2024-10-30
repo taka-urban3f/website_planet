1092e6134c0a9861e7b8d1975a547146d0413d10
taka <taka@users.noreply.github.com>
2024-10-30 13:19:52 +0900

    bgm実装。

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const elem_bgmButton = document.querySelector('.l-header__soundButton');
+if (elem_bgmButton) {
+    console.log('click');
+    elem_bgmButton.addEventListener('click', function () {
+        const elem_bgm = document.querySelector('#bgm');
+        if (elem_bgm) {
+            if (elem_bgmButton.classList.contains('l-header__soundButton--on')) {
+                elem_bgm.pause();
+            } else {
+                elem_bgm.play();
+            }
+
+            elem_bgmButton.classList.toggle('l-header__soundButton--on');
+        }
+    });
+}
